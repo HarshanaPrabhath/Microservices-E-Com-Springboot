01eb0be74cc1c2b51f974d2d3b5f7c9524d93b10
HarshanaPrabhath <harshanaprabhath147@gmail.com>
2025-08-28 00:42:32 +0530

    User Functions Completed

---
+++ microservices/microservices/src/main/java/com/ecom/microservices/Model/User.java
@@ -0,0 +1,13 @@
+package com.ecom.microservices.Model;
+
+
+import lombok.Data;
+
+
+@Data
+public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
