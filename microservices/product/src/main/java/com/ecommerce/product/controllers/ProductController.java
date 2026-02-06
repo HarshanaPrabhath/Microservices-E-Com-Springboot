58d071b06f6ef856732a17974002d44c10b722fa
HarshanaPrabhath <harshanaprabhath147@gmail.com>
2026-02-06 18:18:06 +0530

    microservice product service created

---
+++ microservices/product/src/main/java/com/ecommerce/product/controllers/ProductController.java
@@ -0,0 +1,55 @@
+package com.ecommerce.product.controllers;
+
+
+import com.ecommerce.product.dto.ProductRequest;
+import com.ecommerce.product.dto.ProductResponse;
+import com.ecommerce.product.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products") // plural is best practice
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest productRequest) {
+        ProductResponse response = productService.createProduct(productRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getProducts(@RequestParam(required = false) String name) {
+        if (name != null && !name.isEmpty()) {
+            return ResponseEntity.ok(productService.getByKeyword(name));
+        }
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable Long id) {
+        ProductResponse response = productService.getProductById(id);
+        return (response != null) ? ResponseEntity.ok(response) : ResponseEntity.notFound().build();
+    }
+
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> updateProduct(@PathVariable Long id, @RequestBody ProductRequest productRequest) {
+        ProductResponse response = productService.updateProduct(id, productRequest);
+        return (response != null) ? ResponseEntity.ok(response) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        boolean deleted = productService.deleteProduct(id);
+        return deleted ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
+    }
+}
